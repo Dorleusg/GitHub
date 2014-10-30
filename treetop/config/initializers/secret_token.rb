29b9f95fa7c944f610d94d74b86e509c44b020d8
Greg Dorleus <dorleusg@gmail.com>
2014-10-30 17:40:44 -0400

    Add a Static Pages controller

---
+++ treetop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treetop::Application.config.secret_token = '3e468724afdafd64079f82cd66985462f3b81808aa0c042d81a4a2ad88d9e13d0c183cd02dca6c204fcf72392a917de98fe0286365a88ea93d2bafd61360508d'
